0292f9ca421e89017240e924ab01b59fb39e2fcb
MohammedBetkaoui <mohammed.betkaoui@univ-bba.dz>
2025-02-01 13:23:45 +0100

    delete list from app

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './Components/sidebar/sidebar';
 import Addproduct from './Components/addProduct/AddProduct';
-import ListProduct from './Components/listproduct/ListProduct';
+
 import './index.css';
 
 
@@ -14,7 +14,7 @@ const App = () => {
         <div className="content">
           <Routes>
             <Route path="/addproduct" element={<Addproduct />} />
-            <Route path="/listproduct" element={<ListProduct />} />
+            
             <Route path="/" element={<Addproduct />} /> {/* Par défaut, redirige vers AddProduct */}
           </Routes>
         </div>
